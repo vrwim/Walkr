90f657994974889a486dc9985b8e54e79b5ee1bf
Wim Van Renterghem <git@vrwim.com>
2018-03-12 19:10:03 +0100

    Initial commit

---
+++ Walkr/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Walkr
+//
+//  Created by Wim Van Renterghem on 12/03/2018.
+//  Copyright © 2018 Wim Van Renterghem. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
