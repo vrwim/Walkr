ba6c89f1c89b7a48b6be460188182dadf97fbbcf
Wim Van Renterghem <git@vrwim.com>
2021-03-12 23:22:04 +0100

    Added home screen

--- Shared/ContentView.swift
+++ Shared/ContentView.swift
@@ -9,8 +9,17 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        VStack(spacing: 20) {
+            Text("How this app works")
+                .font(.title)
+            Text("This app can overlay a picture of a route on the map, allowing you to see your location in the route in real time")
+            Button(action: {
+                // TODO: navigationlink to the camera/picture view
+            }, label: {
+                Text("Click here to start")
+            }).padding(20)
+        }
+        .padding(20)
     }
 }
 
