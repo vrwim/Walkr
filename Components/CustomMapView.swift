422e463b73cf49e7580d2bc86f2eb6ca608d5002
Wim Van Renterghem <git@vrwim.com>
2021-03-26 08:52:26 +0100

    Migrate mapview logic to SwiftUI

---
+++ Components/CustomMapView.swift
@@ -0,0 +1,80 @@
+//
+//  CustomMapView.swift
+//  Walkr (iOS)
+//
+//  Created by Wim Van Renterghem on 22/03/2021.
+//
+
+import SwiftUI
+import MapKit
+
+struct CustomMapView: UIViewRepresentable {
+    @Binding var overlays: [ImageOverlay]
+    @Binding var visibleMapRect: MKMapRect
+    @Binding var cameraHeading: CLLocationDirection
+    
+    func makeUIView(context: Context) -> MKMapView {
+        let mapView = MKMapView()
+        mapView.showsUserLocation = true
+        
+        mapView.visibleMapRect = visibleMapRect
+        mapView.camera.heading = cameraHeading
+        
+        mapView.delegate = context.coordinator
+        return mapView
+    }
+
+    func updateUIView(_ view: MKMapView, context: Context) {
+        let addedOverlays = overlays.filter { overlay in
+            !view.overlays.contains { $0 as! NSObject == overlay }
+        }
+        if !addedOverlays.isEmpty {
+            print("Adding overlays: \(addedOverlays)")
+            view.addOverlays(addedOverlays)
+        }
+        
+        let removedOverlays = view.overlays.filter { overlay in
+            !overlays.contains { $0 == overlay as! NSObject }
+        }
+        if !removedOverlays.isEmpty {
+            print("Removing overlays: \(removedOverlays)")
+            view.removeOverlays(removedOverlays)
+        }
+    }
+
+    func makeCoordinator() -> Coordinator {
+        Coordinator(self)
+    }
+    
+    class Coordinator: NSObject, MKMapViewDelegate {
+        var parent: CustomMapView
+
+        func mapView(_ mapView: MKMapView, rendererFor overlay: MKOverlay) -> MKOverlayRenderer {
+            return ImageOverlayRenderer(overlay: overlay)
+        }
+        
+        func mapViewDidChangeVisibleRegion(_ mapView: MKMapView) {
+            parent.visibleMapRect = mapView.visibleMapRect
+            parent.cameraHeading = mapView.camera.heading
+        }
+
+        init(_ parent: CustomMapView) {
+            self.parent = parent
+        }
+    }
+    
+    class ImageOverlayRenderer: MKOverlayRenderer {
+        override func draw(_ mapRect: MKMapRect, zoomScale: MKZoomScale, in context: CGContext) {
+            guard let overlay = overlay as? ImageOverlay else {
+                return
+            }
+            
+            let rect = self.rect(for: overlay.boundingMapRect)
+            
+            context.scaleBy(x: 1.0, y: -1.0)
+            context.translateBy(x: 0.0, y: -rect.size.height)
+            // TODO: rotation??
+            context.draw(overlay.image.cgImage!, in: rect)
+        }
+    }
+}
--- MapView.swift
+++ MapView.swift
@@ -9,9 +9,11 @@ import SwiftUI
 import MapKit
 
 struct MapView: View {
-    @State private var region = MKCoordinateRegion(center: CLLocationCoordinate2D(latitude: 51.507222, longitude: -0.1275), span: MKCoordinateSpan(latitudeDelta: 0.5, longitudeDelta: 0.5))
     @State var image: UIImage?
     @State var pickingFrom: UIImagePickerController.SourceType?
+    @State var photos: [ImageOverlay] = []
+    @State var visibleMapRect = MKMapRect(origin: MKMapPoint(CLLocationCoordinate2D(latitude: 51, longitude: 3)), size: MKMapSize(width: 200_000, height: 200_000))
+    @State var cameraHeading: CLLocationDirection = 0
     
     var body: some View {
         Group {
@@ -20,13 +22,10 @@ struct MapView: View {
                     Image(uiImage: image)
                         .resizable()
                         .aspectRatio(contentMode: .fit)
-                    Map(coordinateRegion: $region)
-                        .ignoresSafeArea()
-                        .opacity(0.5)
-                } else {
-                    Map(coordinateRegion: $region)
-                        .ignoresSafeArea()
                 }
+                CustomMapView(overlays: $photos, visibleMapRect: $visibleMapRect, cameraHeading: $cameraHeading)
+                .ignoresSafeArea()
+                .opacity(image == nil ? 1 : 0.5)
                 VStack {
                     Spacer()
                     HStack(spacing: 20) {
@@ -42,7 +41,20 @@ struct MapView: View {
                         } else {
                             // Aligning image, show done button
                             Button(action: {
-                                // TODO: paste image on map
+                                guard let image = image else {
+                                    fatalError("This button shouldn't be here!")
+                                }
+                                
+                                let imageAspectRatio = Double(image.size.height / image.size.width)
+                                
+                                var mapRect = visibleMapRect
+                                let heightChange = mapRect.size.width * imageAspectRatio - mapRect.size.height
+                                mapRect.size.height = mapRect.size.width * imageAspectRatio
+                                mapRect.origin.y -= heightChange / 2
+                                
+                                photos.append(ImageOverlay(image: image, rect: mapRect, rotation: cameraHeading))
+                                
+                                self.image = nil
                             }, label: {
                                 Text("Done")
                             })
@@ -65,7 +77,7 @@ extension UIImagePickerController.SourceType: Identifiable {
 
 struct MapView_Previews: PreviewProvider {
     static var previews: some View {
-        MapView(image: UIImage(systemName: "circle")!)
+        MapView(image: UIImage(systemName: "circle")!, photos: [])
             .previewDevice("iPhone X")
     }
 }
